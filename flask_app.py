ca16a506b39edb70b56508280939092f282f242e
fareskhlifi <khlifi.fares@supcom.tn>
2023-11-30 21:41:52 +0100

    add files

---
+++ flask_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Fares Khelifi',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'November 30, 2023'
+    },
+    {
+        'author': 'Amine Khelifi',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 30, 2023'
+    }
+]
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
